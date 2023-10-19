154a771073b1220d044c21dd0c0732398bf6a8ec
Alok kumar Yadav <123951091+Alok-2003@users.noreply.github.com>
2023-10-19 08:04:43 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -3,20 +3,20 @@ let ol = document.querySelector("ol");
 let inp = document.querySelector("input");
 let Date = document.querySelector("#date");
 var todo_list = [];
-
+// $(".content").empty();
 btn.addEventListener("click", function () {
+  let box = document.createElement("div.content")
   let item = document.createElement("li");
   let value = inp.value.trim();
   let date = Date.value;
-
-  let contentTask = document.createElement("h1")
-  contentTask.innerText = value;
-  item.appendChild(contentTask);
-
-  let contentDate = document.createElement("p")
-  contentDate.innerText = `Deadline:${date}`;
-  item.appendChild(contentDate);
-
+  item.appendChild(box);
+  let Box = `<div class="content">
+  <h1>${value}</h1>
+  <p>Deadline:<span>${date}</span></p>
+  </div>`;
+  
+  box.innerHTML = Box;
+  
 
   let delBtn = document.createElement("button");
   delBtn.innerText = "Delete";
